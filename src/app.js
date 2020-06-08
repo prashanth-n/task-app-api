fc39af562e6b471e4625fce157e6c7f5aba517cb
Prashanth <prashanth0763@yahoo.com>
2020-06-08 20:08:47 +0530

    added cors.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 require('./db/mongoose.js');
+const cors = require('cors');
 const app = express();
 const userRoutes = require('./routers/user.js');
 const taskRoutes = require('./routers/task.js');
-app.use(express.json())
-app.use(userRoutes)
-app.use(taskRoutes)
+app.use(cors())
+app.use(express.json());
+app.use(userRoutes);
+app.use(taskRoutes);
 module.exports = app;
